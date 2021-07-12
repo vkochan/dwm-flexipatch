432606fcdb83185ebbcc93a4143d7007ae36178b
Vadym Kochan <vadim4j@gmail.com>
2021-07-13 01:28:38 +0300

    patches: enable attachaside patch
    Might be useful for IDE-like workflow.
    
    Signed-off-by: Vadym Kochan <vadim4j@gmail.com>

--- patches.def.h
+++ patches.def.h
@@ -388,7 +388,7 @@
  * This patch takes precedence over ATTACHBELOW_PATCH.
  * https://dwm.suckless.org/patches/attachaside/
  */
-#define ATTACHASIDE_PATCH 0
+#define ATTACHASIDE_PATCH 1
 
 /* This patch adds new clients below the selected client.
  * This patch takes precedence over ATTACHBOTTOM_PATCH.
