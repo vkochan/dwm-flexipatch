4131657684908c4c9d017413794c268e7c147827
Vadym Kochan <vadim4j@gmail.com>
2021-07-17 00:48:43 +0300

    config: add some tag matching rules (firefox, skype, teams, zoom)

--- config.def.h
+++ config.def.h
@@ -413,8 +413,10 @@ static const Rule rules[] = {
 	RULE(.wintype = WTYPE "UTILITY", .isfloating = 1)
 	RULE(.wintype = WTYPE "TOOLBAR", .isfloating = 1)
 	RULE(.wintype = WTYPE "SPLASH", .isfloating = 1)
-	RULE(.class = "Gimp", .tags = 1 << 4)
-	RULE(.class = "Firefox", .tags = 1 << 7)
+	RULE(.class = "firefox", .tags = 1 << 2)
+	RULE(.class = "Skype", .tags = 1 << 3)
+	RULE(.class = "Microsoft Teams - Preview", .tags = 1 << 3)
+	RULE(.class = "zoom", .tags = 1 << 3)
 	#if SCRATCHPADS_PATCH
 	RULE(.instance = "spterm", .tags = SPTAG(0), .isfloating = 1)
 	#endif // SCRATCHPADS_PATCH
